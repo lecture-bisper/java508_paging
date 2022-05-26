22eaec762d31dc98156c9d97f49ebeddafc02707
lecture-bisper <bisper@outlook.kr>
2022-05-26 14:48:17 +0900

    프로젝트 생성.

---
+++ src/test/java/com/bitc/pagingtest/PagingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitc.pagingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagingTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
